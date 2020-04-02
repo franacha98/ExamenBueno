2640b16a423c2f1925779abef71f7db0979ed962
Fran <Fran@DESKTOP-12I0GON>
2020-04-02 17:24:20 +0200

    ejercicio ciudad hecho

--- src/main/java/examen/doc.java
+++ src/main/java/examen/doc.java
@@ -19,5 +19,18 @@ public interface doc
      */
    public abstract String escribirLinea(int x, String line, String file);
    
-   
+   /**
+    * Precondición: tanto la ciudad como el país debe existir en la base de datos, de otro modo, devolverá 0.
+    * Poscondición: devuelve un número tipo double POSITIVO. Devuelve 0 si la ciudad o el país no aparecen en la base de datos de la aplicación
+    * 
+    * Método abstracto que recibe una ciudad (String) y el país al que pertenece la ciudad, por si hubiera ciudades repetidas en diferentes países).<br>
+    * Devuelve un double con el área de dicha ciudad, representado en kilómetros cuadrados
+    * 
+    * @param ciudad Cadena con la ciudad elegida
+    * @param pais Cadena con el país de la ciudad(por si hubiera más paises con la misma ciudad)
+    * @return devuelve un double con el área de la ciudad escogida (en kilómetros cuadrados).
+    * 
+    * 
+    */
+   public abstract double areaCiudad(String ciudad, String pais);
 }
